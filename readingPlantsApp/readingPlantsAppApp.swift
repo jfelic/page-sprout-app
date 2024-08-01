7777e2d860c4b9dc166e509f7f669e219e28b12b
jfelic <felicianojulian1@gmail.com>
2024-08-01 17:55:14 -0400

    Initial Commit

---
+++ readingPlantsApp/readingPlantsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  readingPlantsAppApp.swift
+//  readingPlantsApp
+//
+//  Created by Julian Feliciano on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct readingPlantsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
